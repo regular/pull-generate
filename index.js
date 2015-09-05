2179215c298deda230c54ec0628aa5236056d242
Jan Bölsche <regular.gonzales@googlemail.com>
2015-09-05 16:45:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// jshint -W033, -W030
+
+function abortCb(cb, abort, onAbort) {
+  cb(abort)
+  onAbort && onAbort(abort === true ? null: abort)
+  return
+}
+
+var generate =
+module.exports = function (initialState, expand, onAbort) {
+  var state = initialState
+  return function (abort, cb) {
+      console.log('READ abort=', abort);
+    if(abort)
+      return abortCb(cb, abort, onAbort)
+
+    expand(state, function(err, newState, data) {
+      state = newState
+      cb(err, err ? null : data)
+    })
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,108 @@
+// jshint -W033, -W041
+// originally copied from pull-stream/test/readarray.js
+
+var tape       = require('tape')
+
+var pull      = require('pull-stream')
+var sources    = require('pull-stream/sources')
+var sinks      = require('pull-stream/sinks')
+
+var generate  = require('../')
+
+var pipeableSource = pull.pipeableSource
+var pipeable       = pull.pipeable
+var pipeableSink   = pull.pipeableSink
+
+function arrayReader (read, cb) {
+  var array = []
+  read(null, function next (end, data) {
+    
+    if(end)
+      return cb(end === true ? null : end, array)
+
+    array.push(data)
+    read(null, next)
+  })
+}
+
+tape('basics', function (t) {
+  var read = generate(1, function(state, cb) {
+    cb(state>3 ? true : null, state + 1, state)
+  })
+  read(null, function (e, d) {
+    t.ok(e == null, 'nullish')
+    t.equal(d, 1)
+    read(null, function (e, d) {
+      t.ok(e == null, 'nullish')
+      t.equal(d, 2)
+      read(null, function (e, d) {
+        t.ok(e == null, 'nullish')
+        t.equal(d, 3)
+        read(null, function (e, d) {
+          t.equal(e, true)
+          t.ok(d == null, 'nullish')
+          t.end()
+        })
+      })
+    })
+  })
+})
+
+tape('generate', function (t) {
+  var array = [1, 2, 3]
+  var read = generate(1, function(state, cb) {
+    cb(state>3 ? true : null, state + 1, state)
+  })
+  arrayReader(read, function (err, _array) {
+      console.log('END?')
+      t.deepEqual(_array, array)
+      t.end()
+    })
+})
+
+tape('pipe', function (t) {
+  var array = [1, 2, 3]
+  var read = pipeableSource(generate)(1, function(state, cb) {
+    cb(state>3 ? true : null, state + 1, state)
+  })
+ 
+  t.equal('function', typeof read)
+  t.equal('function', typeof read.pipe)
+
+  read.pipe(pipeableSink(arrayReader)(function (err, _array) {
+    t.equal(err, null)
+    t.deepEqual(_array, array)
+    t.end()
+  }))
+})
+
+tape('pipe2', function (t) {
+  var array = [1, 2, 3]
+  var read = pipeableSource(generate)(1, function(state, cb) {
+    cb(state>3 ? true : null, state + 1, state)
+  })
+  arrayWriter = pull.writeArray
+
+  t.equal('function', typeof read)
+  t.equal('function', typeof read.pipe)
+
+  read
+    .pipe(function (read) {
+      return function (end, cb) {
+        read(end, function (end, data) {
+          console.log(end, data)
+          cb(end, data != null ? data * 2 : null)
+        })
+      }
+    })
+    .pipe(arrayWriter(function (err, _array) {
+      console.log(_array)
+      t.equal(err, null)
+      t.deepEqual(_array, array.map(function (e) {
+        return e * 2
+      }))
+      t.end()
+    }))
+
+})
+
